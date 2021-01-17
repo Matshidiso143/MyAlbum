69e5724e2067f955e1dfba3d730664016dc1e9ed
Matshidiso143 <moshoalibamatshidiso@gmail.com>
2021-01-17 19:08:46 +0200

    Upload

--- Startup.cs
+++ Startup.cs
@@ -62,7 +62,7 @@ namespace MyAlbum
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Image}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
