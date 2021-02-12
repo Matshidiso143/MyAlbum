3ec17661545c27b76900808b27c4dd48ff74525b
Matshidiso143 <moshoalibamatshidiso@gmail.com>
2021-02-12 08:13:49 +0200

    Pattern

---
+++ Models/IAlbum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAlbum.Models
+{
+    interface IAlbum
+    {
+        ImageClass GetImageClass(string image);
+        ImageClass Delete(string image);
+    }
+}
